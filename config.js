2ff65fb98379d5540e11812ac29f81f61fbe82f3
danvogiavinh <99949352+danvogiavinh@users.noreply.github.com>
2022-03-01 10:51:08 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Đôi lời anh muốn nhắn với em';
+const contentCard = 'Vậy là đã đến ngày 8/3 rồi. Anh cũng chẳng biết nói gì nữa. Em quá tuyệt vời, không ngôn từ nào có thể diễn tả được hết. Anh chỉ muốn nói rằng:Chúc em có một ngày 8/3 vui vẻ bên gia đình, chúc Thiện của anh ngày càng xinh đẹp, thành công hơn nữa trong cuộc sống.Cảm ơn em đã ở bên anh suốt thời gian qua! Mãi ở bên anh nhé !';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'thu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
